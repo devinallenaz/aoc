92a73d51a4285b98246e270fee4232498a4d9b28
Devin Allen <dallen@triumphpay.com>
2023-12-11 09:24:37 -0700

    Setup script

---
+++ bin/DayX.cs
@@ -0,0 +1,21 @@
+using AocHelpers;
+using AocHelpers.Solvers;
+public class Day% : Solver
+{
+  public override int Day => %;
+  
+  //Problem 1
+  public override object ExpectedOutput1 => base.ExpectedOutput1;
+
+  public override object Solve1(string input)
+  {
+    return base.Solve1(input);
+  }
+
+  //Problem 2
+  public override object ExpectedOutput2 => base.ExpectedOutput2;
+  public override object Solve2(string input)
+  {
+    return base.Solve2(input);
+  }
+}
