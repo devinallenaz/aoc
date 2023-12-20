6da06b1e6934d6faa1a16a9acdd7f5759a54f216
Devin Allen <dallen@triumphpay.com>
2023-12-20 09:41:35 -0700

    2023 - Day 18 - Refactor and part 2

--- Aoc2023/Models/Digger.cs
+++ Aoc2023/Models/Digger.cs
@@ -5,109 +5,137 @@ using AocHelpers;
 
 namespace Aoc2023.Models;
 
-public enum CornerType
-{
-    None,
-    UL,
-    UR,
-    DL,
-    DR,
-}
-
 public class Digger
 {
     private List<DiggerInstruction> Instructions { get; }
 
 
-    public Digger(string init)
+    public Digger(string init, bool decode = false)
     {
-        this.Instructions = init.SplitLines().Select(s => new DiggerInstruction(s)).ToList();
+        this.Instructions = init.SplitLines().Select(s => new DiggerInstruction(s, decode)).ToList();
     }
 
-    public List<DugPoint> Dig()
+    public List<(long x, long y)> Dig()
     {
-        (int x, int y) currentPosition = (0, 0);
-        var output = new List<DugPoint>();
-
+        (long x, long y) currentPosition = (0, 0);
+        var output = new List<(long x, long y)>();
+        bool bottomEdge = false;
+        bool rightEdge = false;
         for (var i = 0; i < this.Instructions.Count; i++)
         {
-            var diggerInstruction = this.Instructions[i];
-            var cornerType = CornerType.None;
-            for (int j = 0; j < diggerInstruction.Count; j++)
+            var current = this.Instructions[i];
+            var next = this.Instructions[(i + 1) % this.Instructions.Count];
+            var distance = current.Count;
+            switch (current.DirectionName, next.DirectionName, rightEdge, bottomEdge)
             {
-                if (j == diggerInstruction.Count - 1)
+                case ("R", "D", false, false):
+                    distance = distance + 1;
+                    rightEdge = true;
+                    break;
+
+                case ("D", "L", true, false):
+                    distance = distance + 1;
+                    bottomEdge = true;
+                    break;
+                case ("D", "R", false, true):
+                    distance = distance + 1;
+                    bottomEdge = false;
+                    break;
+                case ("L", "U", true, true):
+                    distance = distance + 1;
+                    rightEdge = false;
+                    break;
+                case ("U", "R", false, true):
                 {
-                    cornerType = this.CalculateCornerType(diggerInstruction, this.Instructions[(i + 1) % this.Instructions.Count]);
+                    distance = distance + 1;
+                    bottomEdge = false;
+                    break;
                 }
 
-                currentPosition = currentPosition.Plus(diggerInstruction.Direction);
-                output.Add(new DugPoint(currentPosition.x, currentPosition.y, diggerInstruction.Color, cornerType));
+                case ("U", "L", false, false):
+                    distance = distance - 1;
+                    bottomEdge = true;
+                    break;
+                case ("L", "D", false, true):
+                    distance = distance - 1;
+                    rightEdge = true;
+                    break;
+                case ("R", "U", true, false):
+                    distance = distance - 1;
+                    rightEdge = false;
+                    break;
+                case ("D", "L", false, false):
+                    distance = distance - 1;
+                    bottomEdge = true;
+                    break;
+                case ("D", "R", true, true):
+                    distance = distance - 1;
+                    bottomEdge = false;
+                    break;
+                case ("R", "D", true, true):
+                    distance = distance - 1;
+                    rightEdge = false;
+                    break;
             }
+
+
+            currentPosition = currentPosition.Plus((current.Direction.x * distance, current.Direction.y * distance));
+
+
+            output.Add(currentPosition);
         }
 
         return output;
     }
 
-    private CornerType CalculateCornerType(DiggerInstruction current, DiggerInstruction next)
+    public class DiggerInstruction
     {
-        switch (current.Direction, next.Direction)
+        private static Regex _regex = new Regex(@"([UDLR]) (\d+) \(#(\w\w\w\w\w\w)\)");
+        public (long x, long y) Direction { get; }
+        public string DirectionName { get; }
+        public long Count { get; }
+
+        public DiggerInstruction(string init, bool decode = false)
         {
-            case ((1, 0), (0, 1)):
-                return CornerType.DL;
-            case ((1, 0), (0, -1)):
-                return CornerType.UL;
-            case ((-1, 0), (0, 1)):
-                return CornerType.DR;
-            case ((-1, 0), (0, -1)):
-                return CornerType.UR;
-            case ((0, 1), (1, 0)):
-                return CornerType.UR;
-            case ((0, 1), (-1, 0)):
-                return CornerType.UL;
-            case ((0, -1), (1, 0)):
-                return CornerType.DR;
-            case ((0, -1), (-1, 0)):
-                return CornerType.DL;
-        }
-
-        throw new InvalidOperationException();
-    }
-}
-
-public class DiggerInstruction
-{
-    private static Regex _regex = new Regex(@"([UDLR]) (\d+) \(#(\w\w\w\w\w\w)\)");
-    public (int, int) Direction { get; }
-    public int Count { get; }
-
-    public DiggerInstruction(string init, bool decode = false)
-    {
-        var match = _regex.Match(init);
-        var groups = match.Groups;
-        if (!decode)
-        {
-            this.Direction = groups[1].Value switch
+            var match = _regex.Match(init);
+            var groups = match.Groups;
+            if (!decode)
             {
-                "U" => Points.Up,
-                "D" => Points.Down,
-                "R" => Points.Right,
-                "L" => Points.Left,
-                _ => throw new InvalidOperationException(),
-            };
-            this.Count = int.Parse(groups[2].Value);
-        }
-        else
-        {
-            var encoded = groups[3].Value;
-            var distance = encoded.Substring(0, 5);
-            var direction = encoded.Substring(5, 1);
-            this.Count = int.Parse(distance, NumberStyles.HexNumber);
-        }
+                this.Direction = groups[1].Value switch
+                {
+                    "U" => Points.Up,
+                    "D" => Points.Down,
+                    "R" => Points.Right,
+                    "L" => Points.Left,
+                    _ => throw new InvalidOperationException(),
+                };
+                this.DirectionName = groups[1].Value;
+                this.Count = long.Parse(groups[2].Value);
+            }
+            else
+            {
+                var encoded = groups[3].Value;
+                var distance = encoded.Substring(0, 5);
+                var direction = encoded.Substring(5, 1);
 
-        
+                this.Direction = direction switch
+                {
+                    "0" => Points.Right,
+                    "1" => Points.Down,
+                    "2" => Points.Left,
+                    "3" => Points.Up,
+                    _ => throw new InvalidOperationException(),
+                };
+                this.DirectionName = direction switch
+                {
+                    "0" => "R",
+                    "1" => "D",
+                    "2" => "L",
+                    "3" => "U",
+                    _ => throw new InvalidOperationException(),
+                };
+                this.Count = long.Parse(distance, NumberStyles.HexNumber);
+            }
+        }
     }
-}
-
-public record DugPoint(int X, int Y, string Color, CornerType CornerType)
-{
 }
--- Aoc2023/Solvers/Day18.cs
+++ Aoc2023/Solvers/Day18.cs
@@ -7,63 +7,17 @@ public class Day18 : Solver
     public override int Day => 18;
 
     //Problem 1
-    public override object ExpectedOutput1 => 62;
+    public override object ExpectedOutput1 => 62L;
 
-    private int CalculateDugArea(List<DugPoint> points)
+    private long CalculateDugArea(List<(long x, long y)> points)
     {
-        var minX = points.Min(p => p.X);
-        var maxX = points.Max(p => p.X);
-        var minY = points.Min(p => p.Y);
-        var maxY = points.Max(p => p.Y);
-        var area = 0;
-        for (int x = minX; x <= maxX; x++)
-        {
-            var inside = false;
-            var previousCorner = (CornerType?)null;
-            for (int y = minY; y <= maxY; y++)
-            {
-                var point = points.FirstOrDefault(p => p.X == x && p.Y == y);
-                if (point != null)
-                {
-                    switch (previousCorner, point.CornerType)
-                    {
-                        case (null, CornerType.None):
-                            inside = !inside;
-                            break;
-                        case (_,CornerType.None):
-                            break;
-                        case (null, CornerType.DL):
-                        case (null, CornerType.DR):
-                            inside = !inside;
-                            previousCorner = point.CornerType;
-                            break;
-                        case (CornerType.DL, CornerType.UL):
-                            inside = !inside;
-                            previousCorner = null;
-                            break;
-                        case (CornerType.DL, CornerType.UR):
-                            previousCorner = null;
-                            break;
-                        case (CornerType.DR, CornerType.UR):
-                            inside = !inside;
-                            previousCorner = null;
-                            break;
-                        case (CornerType.DR, CornerType.UL):
-                            previousCorner = null;
-                            break;
-                        default:
-                            throw new NotImplementedException();
-                    }
-                }
+        List<(double x, double y)> doublePoints = points.Select(p => ((double)p.x, (double)p.y)).ToList();
+        doublePoints.Add(doublePoints[0]);
+        var area = Math.Abs(doublePoints.Take(doublePoints.Count - 1)
+            .Select((p, i) => (doublePoints[i + 1].x - p.x) * (doublePoints[i + 1].y + p.y))
+            .Sum() / 2);
 
-                if (inside || point != null)
-                {
-                    area++;
-                }
-            }
-        }
-
-        return area;
+        return (long)area;
     }
 
 
@@ -75,10 +29,13 @@ public class Day18 : Solver
     }
 
     //Problem 2
-    public override object ExpectedOutput2 => base.ExpectedOutput2;
+    public override object ExpectedOutput2 => 952408144115L;
 
     public override object Solve2(string input)
     {
-        return base.Solve2(input);
+        
+        var digger = new Digger(input,true);
+        var dugPoints = digger.Dig();
+        return CalculateDugArea(dugPoints);
     }
 }
