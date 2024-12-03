57c643c7ba8f147ac2c74659fe76e3e5b6b30982
Devin Allen <dallen@triumphpay.com>
2024-12-03 09:51:58 -0700

    2024 - Day 3

---
+++ Aoc2024/Solvers/Day3.cs
@@ -0,0 +1,29 @@
+using System.Text.RegularExpressions;
+using AocHelpers;
+using AocHelpers.Solvers;
+public class Day3 : Solver
+{
+  public override int Day => 3;
+  
+  //Problem 1
+  public override object ExpectedOutput1 => 161;
+
+  public override object Solve1(string input)
+  {
+    return FindMultiples(input);
+  }
+
+  private int FindMultiples(string input)
+  { 
+    var matches = new Regex(@"mul\((\d\d?\d?),(\d\d?\d?)\)").Matches(input);
+    return matches.Sum(m => int.Parse(m.Groups[1].Value) * int.Parse(m.Groups[2].Value));
+  }
+
+  //Problem 2
+  public override object ExpectedOutput2 => 48;
+  public override object Solve2(string input)
+  {
+    var improved = new Regex(@"don't\(\)(.|\n)*?((do\(\))|$)").Replace(input, "x");
+    return FindMultiples(improved);
+  }
+}
--- AocHelpers/Input/InputHelper.cs
+++ AocHelpers/Input/InputHelper.cs
@@ -6,7 +6,7 @@ public static class InputHelper
 {
     public static string GetInputText(int day, bool test = false, bool problem2 = false)
     {
-        if (test && problem2 && File.Exists($"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}/dallen/projects/Aoc/Aoc{Runner.Year}/Input/day{day}_test_2.txt"))
+        if (test && problem2 && File.Exists($"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}/projects/Aoc/Aoc{Runner.Year}/Input/day{day}_test_2.txt"))
         {
             return File.ReadAllText($"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}/projects/Aoc/Aoc{Runner.Year}/Input/day{day}_test_2.txt");
         }
