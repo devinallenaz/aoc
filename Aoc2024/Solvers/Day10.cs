8e7a0afaa9e403295a8168722430a22767fa8f03
Devin Allen <dallen@triumphpay.com>
2024-12-10 09:20:33 -0700

    prefere enumerable

--- Aoc2024/Solvers/Day10.cs
+++ Aoc2024/Solvers/Day10.cs
@@ -8,17 +8,18 @@ public class Day10 : Solver
 
     //Problem 1
     public override object ExpectedOutput1 => 36;
-    private static List<Point> ReachableNines(int[,] grid, Point current, int currentValue)
+    private static IEnumerable<Point> ReachableNines(int[,] grid, Point current, int currentValue)
     {
         if (currentValue == 9)
         {
             return [current];
         }
 
-
         var nextValue = currentValue + 1;
-
-        var reachable = current.AdjacentPointsCardinal().Select(p => (p, grid.ValueOrNull(p.x, p.y))).Where(a => a.Item2 == nextValue).SelectMany(a => ReachableNines(grid, a.p, nextValue)).Distinct().ToList();
+        var reachable = current.AdjacentPointsCardinal()
+            .Where(next => grid.ValueOrNull(next.x, next.y) == nextValue)
+            .SelectMany(next => ReachableNines(grid, next, nextValue))
+            .Distinct();
         
         return reachable;
     }
@@ -41,7 +42,7 @@ public class Day10 : Solver
     {
         var grid = input.To2dIntArray();
         var startingPoints = grid.FindAll(0);
-        return startingPoints.Sum(p => ReachableNines(grid, p, 0).Count);
+        return startingPoints.Sum(p => ReachableNines(grid, p, 0).Count());
     }
 
     //Problem 2
