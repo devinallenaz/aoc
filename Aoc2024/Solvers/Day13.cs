c135ae96bb6d2bb5077a5020be29a7d8e5651058
Devin Allen <dallen@triumphpay.com>
2024-12-13 11:23:39 -0700

    Cleaned up math comment

--- Aoc2024/Solvers/Day13.cs
+++ Aoc2024/Solvers/Day13.cs
@@ -25,29 +25,17 @@ public partial class Day13 : Solver
 
     private static long MinimumCostForPrize(Claw claw)
     {
-        //ax*a+bx*b=tx
-        //ay*a+by*b=ty
-        //a=(ty-(by*b))/ay
-        //ax*((ty-(by*b))/ay) + bx*b = tx
-        //ax*(ty/ay - (by/ay)b) + bx*b = tx
-        //(ax*ty/ay)- (ax*by/ay)b + bx*b = tx
-        //((ax*ty)- (ax*by)b)/ay = tx
-        //(ax*ty)- (ax*by)b = ay*tx
-        //(ax*ty) - (ay*tx) = (ax*by)b
-        //(ax*by)b = (ax*ty) - (ay*tx)
-        //b = ((ax*ty) - (ay*tx))/(ax*by)
-
-        //94a + 22b = 8400
-        //34a + 67b = 5400
-        //34a = 5400 - 67b
-        //a = (5400 -67b)/34
-        //94*((5400 -67b)/34) + 22b = 8400
-        //94*(5400/34 - (67/34)b)+22b = 8400
-        //(94*5400/34) - (94*67/34)b + 22b = 8400
-        //(94*5400/34) + 22b - (94*67/34)b = 8400
-        //22b - (94*67/34)b = 8400 - (94*5400/34)
-        //b(22-(94*67/34)) = 8400 - (94*5400/34)
-        //b = (8400 - (94*5400/34))/(22-(94*67/34))
+        //ax*a + bx*b = tx
+        //ay*a + by*b = ty
+        //ay*a = ty - by*b
+        //a = (ty -by*b)/ay
+        //ax*((ty -by*b)/ay) + bx*b = tx
+        //ax*(ty/ay - (by/ay)b)+bx*b = tx
+        //(ax*ty/ay) - (ax*by/ay)b + bx*b = tx
+        //(ax*ty/ay) + bx*b - (ax*by/ay)b = tx
+        //bx*b - (ax*by/ay)b = tx - (ax*ty/ay)
+        //b(bx-(ax*by/ay)) = tx - (ax*ty/ay)
+        //b = (tx - (ax*ty/ay))/(bx-(ax*by/ay))
         var b = (claw.tx - (claw.ax * claw.ty / claw.ay)) / (claw.bx - (claw.ax * claw.by / claw.ay));
         var a = (claw.ty - (claw.by * b)) / claw.ay;
         if (a > 0 && b > 0 && a.RoundsToWholeNumber() && b.RoundsToWholeNumber())
